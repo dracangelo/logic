9bc038c579c282deb1fe557396bfdcb1eebd2c59
Angelo <kelvinmbuguaw@gmail.com>
2020-07-08 00:23:54 +0300

    first commit

--- blog/views.py
+++ blog/views.py
@@ -19,7 +19,7 @@ def post_list(request):
             Q(tags__name__icontains= search_query)
         ).distinct()
 
-    paginator = Paginator(post_list, 2) # Show 2 posts per page
+    paginator = Paginator(post_list, 2) # Show 25 posts per page
 
     page = request.GET.get('page')
     post_list = paginator.get_page(page)
@@ -30,7 +30,7 @@ def post_list(request):
     return render(request , 'post/blog.html' , context)
 
 
-def post_detail(request, id):
+def post_detail(request , id):
     post_detail = Post.objects.get(id=id)
     categories = Category.objects.all()
     all_tags = Tag.objects.all()
@@ -56,7 +56,7 @@ def post_detail(request, id):
         'comment_form' : comment_form
     }
 
-    return render(request , 'post/blog_details.html' , context)
+    return render(request , 'post/blog_detail.html' , context)
 
 def post_by_tag(request , tag):
     post_by_tag = Post.objects.filter(tags__name__in=[tag])
@@ -64,7 +64,7 @@ def post_by_tag(request , tag):
         'post_list' : post_by_tag ,
     }
 
-    return render(request , 'Post/blog.html' , context)
+    return render(request , 'post/blog.html' , context)
 
 def post_by_category(request , category):
     post_by_category = Post.objects.filter(category__category_name=category)
@@ -72,4 +72,4 @@ def post_by_category(request , category):
         'post_list' : post_by_category ,
     }
 
-    return render(request , 'Post/blog.html' , context)
+    return render(request , 'post/blog.html' , context)
