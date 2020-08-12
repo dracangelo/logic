4225fab383c7ff9b25c2fb49711509dce05e8264
Angelo <kelvinmbuguaw@gmail.com>
2020-08-12 14:45:40 +0300

    new commit

--- logisticexpress/settings.py
+++ logisticexpress/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'logisticexpress.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'leni',
+        'USER': 'postgres',
+        'PASSWORD':'securedd@2',
+        'HOST':'localhost',
+        'PORT':'8000',
     }
 }
 
